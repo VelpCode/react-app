f896b5bfb88db3513dc20f882bc1905d1d98309e
Aman Velpula <velpmail@gmail.com>
2023-04-18 18:30:34 -0400

    using tailwind features for overall site

--- react-app/src/pages/index.js
+++ react-app/src/pages/index.js
@@ -1,6 +1,7 @@
 import Head from "next/head";
 
-
+import {BsFillMoonStarsFill} from 'react-icons/bs';
+import {AiFillTwitterCircle, AiFillLinkedin, AiFillYoutube} from "react-icons/ai"
 
 export default function Home() {
   return (
@@ -11,7 +12,34 @@ export default function Home() {
         <link rel = "icon" href = "/favicon.ico"></link>
       </Head>
 
-      <main></main>
+      <main className ="bg-white px-10">
+        <section className="min-h-screen">
+          <nav className = "p-10 mb-12 flex justify-between">
+            <h1 className ="text-xl font-burtons">Velp Doodles</h1>
+            <ul className = "flex items-center">
+              <li>
+                <BsFillMoonStarsFill className = "cursor-pointer text-2xl"/>
+              </li>
+              <li><a className = "bg-gradient-to-r from-cyan-500 to-teal-500 text-white px-4 py-2 border-none rounded-md ml-8" href="#">Resume</a></li>
+            </ul>
+          </nav>
+          <div className = "text-center p-10">
+            <h2 className ="text-5xl py2 text-teal-600 font-medium">Aman Velp</h2>
+            <h3 className = "text-2xl py-2">Frontend Developer and fulltime doodler.</h3>
+            <p className = "text-md py-5 leading-8 text-gray-800">I'm a university student studying Computer Science that has a passion for
+              building, learning & fitness. Find out more about me!
+            </p>
+          </div>
+          <div className = "text-5xl flex justify-center gap-16">
+            <AiFillTwitterCircle />
+            <AiFillLinkedin />
+            <AiFillYoutube />
+          </div>
+          <div className = ""
+
+
+        </section>
+      </main>
     </div>
   )
 }
